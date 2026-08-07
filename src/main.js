2b555a9420c34b8e908b7f2713d49bc23910b293
Paulo Junior <paulonp@proton.me>
2026-08-06 21:36:21 -0300

    add alpinejs plus basic nasa api fetch

--- src/main.js
+++ src/main.js
@@ -1,5 +1,28 @@
+import Alpine from "alpinejs";
+
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-a
-`;
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+Alpine.data("apod", () => ({
+  loading: true,
+  data: null,
+  error: null,
+
+  async init() {
+    try {
+      const response = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,
+      );
+      if (!response.ok) throw new Error(`HTTP ${response.status}`);
+      this.data = await response.json();
+    } catch (e) {
+      this.error = e.message;
+    } finally {
+      this.loading = false;
+    }
+  },
+}));
+
+window.Alpine = Alpine;
+Alpine.start();
