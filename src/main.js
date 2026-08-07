4d258bbc149237ab54e60e7d947067fa5e650f30
Paulo Junior <paulonp@proton.me>
2026-08-07 00:17:09 -0300

    add cache to nasa api fetch

--- src/main.js
+++ src/main.js
@@ -10,14 +10,45 @@ Alpine.data("apod", () => ({
   error: null,
 
   async init() {
+    let savedAt = localStorage.getItem("savedAt:apod:v1");
+    let needsToFetch =
+      savedAt == null || Date.now() - Date.parse(savedAt) >= 60 * 60 * 1000;
+
+    let savedTitle = localStorage.getItem("title:apod:v1");
+    let savedUrl = localStorage.getItem("url:apod:v1");
+    let savedExplanation = localStorage.getItem("explanation:apod:v1");
+
     try {
-      const response = await fetch(
-        `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,
-      );
-      if (!response.ok) throw new Error(`HTTP ${response.status}`);
-      this.data = await response.json();
+      if (!needsToFetch) {
+        this.data = {};
+        this.data.title = savedTitle;
+        this.data.url = savedUrl;
+        this.data.explanation = savedExplanation;
+      } else {
+        const response = await fetch(
+          `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`,
+        );
+
+        if (!response.ok) throw new Error(`HTTP ${response.status}`);
+
+        this.data = await response.json();
+
+        localStorage.setItem("title:apod:v1", this.data.title);
+        localStorage.setItem("url:apod:v1", this.data.url);
+        localStorage.setItem("explanation:apod:v1", this.data.explanation);
+        localStorage.setItem("savedAt:apod:v1", new Date().toISOString());
+      }
     } catch (e) {
-      this.error = e.message;
+      if (savedTitle != null) {
+        this.data = {
+          title: savedTitle,
+          url: savedUrl,
+          explanation: savedExplanation,
+        };
+      } else {
+        this.error = e.message;
+        await init();
+      }
     } finally {
       this.loading = false;
     }
